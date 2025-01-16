09c8b582236b17b40a47aa826a19d8ad777e8547
johan <johan_lc@tesch.edu.mx>
2025-01-15 18:34:30 -0600

    Guardar registros

--- app/src/main/java/com/example/timecatcher/ui/login/RegisterActivity.kt
+++ app/src/main/java/com/example/timecatcher/ui/login/RegisterActivity.kt
@@ -1,18 +1,53 @@
 package com.example.timecatcher.ui.register
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.timecatcher.databinding.ActivityLoginBinding
+import com.example.timecatcher.databinding.ActivityRegisterBinding
+import com.example.timecatcher.ui.main.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Aquí puedes manejar las interacciones de los elementos de tu registro
+        auth = FirebaseAuth.getInstance()
+
+        // Botón Registrar
+        binding.btnRegisterUser.setOnClickListener {
+            val email = binding.etRegisterEmail.text.toString().trim()
+            val password = binding.etRegisterPassword.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                if (password.length >= 6) {
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show()
+                                goToMain()
+                            } else {
+                                Toast.makeText(this, "Error: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun goToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
